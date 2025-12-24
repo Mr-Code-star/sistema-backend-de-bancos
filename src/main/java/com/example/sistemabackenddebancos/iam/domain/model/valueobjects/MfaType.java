03694d1f6192634fb8c2b6a182d3ba2f4e8ccbce
Mr-Code-start <vitalybaca92@gmail.com>
2025-12-24 18:54:00 -0500

    feat(iam): add Value Object MfaType

---
+++ src/main/java/com/example/sistemabackenddebancos/iam/domain/model/valueobjects/MfaType.java
@@ -0,0 +1,10 @@
+package com.example.sistemabackenddebancos.iam.domain.model.valueobjects;
+
+/**
+ * Tipos de autenticación de segundo factor permitidos.
+ */
+public enum MfaType {
+    EMAIL,
+    SMS,
+    AUTH_APP
+}
