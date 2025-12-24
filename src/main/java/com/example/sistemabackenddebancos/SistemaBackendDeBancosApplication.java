06d17ec1000928482b0b1a81c1fbe4658298f92b
Mr-Code-start <vitalybaca92@gmail.com>
2025-12-24 18:13:28 -0500

    Initial commit

---
+++ src/main/java/com/example/sistemabackenddebancos/SistemaBackendDeBancosApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistemabackenddebancos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaBackendDeBancosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaBackendDeBancosApplication.class, args);
+    }
+
+}
