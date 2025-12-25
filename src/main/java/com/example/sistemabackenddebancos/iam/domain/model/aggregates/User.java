02e9801721fe58bc00fb3e18be2d9b713ea93680
Mr-Code-start <vitalybaca92@gmail.com>
2025-12-24 19:36:13 -0500

    feat(iam): add aggregate User

---
+++ src/main/java/com/example/sistemabackenddebancos/iam/domain/model/aggregates/User.java
@@ -0,0 +1,57 @@
+package com.example.sistemabackenddebancos.iam.domain.model.aggregates;
+
+import com.example.sistemabackenddebancos.iam.domain.model.enumerations.UserStatus;
+import com.example.sistemabackenddebancos.iam.domain.model.valueobjects.Email;
+import com.example.sistemabackenddebancos.iam.domain.model.entities.MfaMethod;
+import com.example.sistemabackenddebancos.iam.domain.model.valueobjects.PasswordHash;
+import com.example.sistemabackenddebancos.iam.domain.model.valueobjects.UserId;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+public class User {
+
+    private final UserId id;
+    private final Email email;
+    private final PasswordHash passwordHash;
+
+    private final UserStatus status;
+    private final boolean mfaEnabled;
+
+    private final List<MfaMethod> mfaMethods;
+    private final int failedAttempts;
+
+    public User(
+            UserId id,
+            Email email,
+            PasswordHash passwordHash,
+            UserStatus status,
+            boolean mfaEnabled,
+            List<MfaMethod> mfaMethods,
+            int failedAttempts
+    ) {
+        this.id = Objects.requireNonNull(id, "User.id cannot be null");
+        this.email = Objects.requireNonNull(email, "User.email cannot be null");
+        this.passwordHash = Objects.requireNonNull(passwordHash, "User.passwordHash cannot be null");
+        this.status = Objects.requireNonNull(status, "User.status cannot be null");
+
+        if (failedAttempts < 0) throw new IllegalArgumentException("failedAttempts cannot be negative");
+
+        this.mfaEnabled = mfaEnabled;
+        this.failedAttempts = failedAttempts;
+
+        // defensivo: copia inmutable
+        List<MfaMethod> safe = (mfaMethods == null) ? new ArrayList<>() : new ArrayList<>(mfaMethods);
+        this.mfaMethods = Collections.unmodifiableList(safe);
+    }
+
+    public UserId id() { return id; }
+    public Email email() { return email; }
+    public PasswordHash passwordHash() { return passwordHash; }
+    public UserStatus status() { return status; }
+    public boolean mfaEnabled() { return mfaEnabled; }
+    public List<MfaMethod> mfaMethods() { return mfaMethods; }
+    public int failedAttempts() { return failedAttempts; }
+}
