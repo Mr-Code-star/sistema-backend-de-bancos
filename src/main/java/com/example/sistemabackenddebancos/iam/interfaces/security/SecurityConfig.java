e9609b3772552373f9e460d4e5575cf6492626e4
Mr-Code-start <vitalybaca92@gmail.com>
2025-12-25 13:19:59 -0500

    feat(interfaces): add security Config

---
+++ src/main/java/com/example/sistemabackenddebancos/iam/interfaces/security/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.example.sistemabackenddebancos.iam.interfaces.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v1/iam/auth/**", "/swagger-ui/**", "/v3/api-docs/**", "/api/v1/health").permitAll()
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+}
