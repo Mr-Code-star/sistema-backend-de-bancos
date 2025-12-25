e3ed01c390c260a34534729f773bb1a139abbce4
Mr-Code-start <vitalybaca92@gmail.com>
2025-12-25 13:29:08 -0500

    feat: initial

--- src/main/java/com/example/sistemabackenddebancos/SistemaBackendDeBancosApplication.java
+++ src/main/java/com/example/sistemabackenddebancos/SistemaBackendDeBancosApplication.java
@@ -2,12 +2,13 @@ package com.example.sistemabackenddebancos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.sistemabackenddebancos")
 public class SistemaBackendDeBancosApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SistemaBackendDeBancosApplication.class, args);
     }
-
 }
