a6976cca7ed9aaf40d6418cf32ca744d49a7b06c
Mr-Code-start <vitalybaca92@gmail.com>
2025-12-26 13:03:01 -0500

    feat: delete

--- src/main/java/com/example/sistemabackenddebancos/iam/application/security/mfa/TotpService.java
+++
@@ -1,7 +0,0 @@
-package com.example.sistemabackenddebancos.iam.application.security.mfa;
-
-public interface TotpService {
-    String generateSecret();
-    String buildQrUri(String issuer, String accountName, String secret);
-    boolean verifyCode(String secret, String code);
-}
