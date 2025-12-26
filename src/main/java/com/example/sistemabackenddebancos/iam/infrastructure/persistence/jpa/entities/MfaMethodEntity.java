50c5a514f15bdf1be3e1d5bbad1bfc141273faa2
Mr-Code-start <vitalybaca92@gmail.com>
2025-12-26 01:05:00 -0500

    feat: delete secret

--- src/main/java/com/example/sistemabackenddebancos/iam/infrastructure/persistence/jpa/entities/MfaMethodEntity.java
+++ src/main/java/com/example/sistemabackenddebancos/iam/infrastructure/persistence/jpa/entities/MfaMethodEntity.java
@@ -27,12 +27,7 @@ public class MfaMethodEntity {
     @JoinColumn(name = "user_id", nullable = false)
     private UserEntity user;
 
-    @Column(name = "secret")
-    private String secret;
-
-    public String getSecret() { return secret; }
-    public void setSecret(String secret) { this.secret = secret; }
-
+    // Eliminamos el campo secret
 
     public MfaMethodEntity() {}
 
