aceba65b520ca89f17c494b33f8259304d7f87b2
Mr-Code-start <vitalybaca92@gmail.com>
2025-12-30 13:04:18 -0500

    feat(accounts): delete ownerId to create OpenAccountRequest

--- src/main/java/com/example/sistemabackenddebancos/accounts/interfaces/rest/dtos/requests/OpenAccountRequest.java
+++ src/main/java/com/example/sistemabackenddebancos/accounts/interfaces/rest/dtos/requests/OpenAccountRequest.java
@@ -1,7 +1,6 @@
 package com.example.sistemabackenddebancos.accounts.interfaces.rest.dtos.requests;
 
 public record OpenAccountRequest(
-        String ownerId,   // UUID del user (IAM)
         String type,      // SAVINGS / CHECKING
         String currency   // PEN / USD
 ) {}
