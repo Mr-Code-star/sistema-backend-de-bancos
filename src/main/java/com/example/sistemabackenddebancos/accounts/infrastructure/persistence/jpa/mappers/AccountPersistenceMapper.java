eb6dc8ed6cd1ff41ba78c1ca537df4399011dfeb
Mr-Code-start <vitalybaca92@gmail.com>
2025-12-30 12:16:27 -0500

    feat(accounts): add persistence mapper between BankAccount domain and JPA entities

---
+++ src/main/java/com/example/sistemabackenddebancos/accounts/infrastructure/persistence/jpa/mappers/AccountPersistenceMapper.java
@@ -0,0 +1,36 @@
+package com.example.sistemabackenddebancos.accounts.infrastructure.persistence.jpa.mappers;
+
+
+import com.example.sistemabackenddebancos.accounts.domain.model.aggregates.BankAccount;
+import com.example.sistemabackenddebancos.accounts.domain.model.valueobjects.AccountId;
+import com.example.sistemabackenddebancos.accounts.domain.model.valueobjects.AccountNumber;
+import com.example.sistemabackenddebancos.accounts.domain.model.valueobjects.Money;
+import com.example.sistemabackenddebancos.accounts.domain.model.valueobjects.OwnerId;
+import com.example.sistemabackenddebancos.accounts.infrastructure.persistence.jpa.entities.AccountEntity;
+
+public class AccountPersistenceMapper {
+
+    public static BankAccount toDomain(AccountEntity e) {
+        return new BankAccount(
+                new AccountId(e.getId()),
+                new OwnerId(e.getOwnerId()),
+                new AccountNumber(e.getAccountNumber()),
+                e.getType(),
+                e.getStatus(),
+                e.getCurrency(),
+                new Money(e.getBalance(), e.getCurrency())
+        );
+    }
+
+    public static AccountEntity toEntity(BankAccount a) {
+        var e = new AccountEntity();
+        e.setId(a.id().value());
+        e.setOwnerId(a.ownerId().value());
+        e.setAccountNumber(a.accountNumber().value());
+        e.setType(a.type());
+        e.setStatus(a.status());
+        e.setCurrency(a.currency());
+        e.setBalance(a.balance().amount());
+        return e;
+    }
+}
