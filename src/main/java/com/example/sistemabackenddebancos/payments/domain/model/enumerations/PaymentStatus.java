939abee4cb218c7de04e354d2d4a2e88bdb080ba
Mr-Code-start <vitalybaca92@gmail.com>
2026-01-02 11:59:20 -0500

    feat(payments): add enumerations

---
+++ src/main/java/com/example/sistemabackenddebancos/payments/domain/model/enumerations/PaymentStatus.java
@@ -0,0 +1,7 @@
+package com.example.sistemabackenddebancos.payments.domain.model.enumerations;
+
+public enum PaymentStatus {
+    PENDING,
+    COMPLETED,
+    FAILED
+}
---
+++ src/main/java/com/example/sistemabackenddebancos/payments/domain/model/enumerations/PaymentType.java
@@ -0,0 +1,6 @@
+package com.example.sistemabackenddebancos.payments.domain.model.enumerations;
+
+public enum PaymentType {
+    SERVICE,
+    MERCHANT
+}
